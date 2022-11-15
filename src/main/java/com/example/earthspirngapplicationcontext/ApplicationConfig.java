c59da8e534bc832a6f719165f255d52c182f751d
jisuuuu <gjkl55@naver.com>
2022-11-15 21:21:35 +0900

    스프링 프레임 워크 핵심 기술 - ApplicationContext와 다양한 빈 설정 방법

---
+++ src/main/java/com/example/earthspirngapplicationcontext/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.example.earthspirngapplicationcontext;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = EarthSpirngApplicationContextApplication.class)
+public class ApplicationConfig {
+}
---
+++ src/test/java/com/example/earthspirngapplicationcontext/EarthSpirngApplicationContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.earthspirngapplicationcontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthSpirngApplicationContextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
